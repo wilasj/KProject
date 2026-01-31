605a58dcc121f1bbdb80db83bf599a294a0a68de
William Alves <willoandrade29@gmail.com>
2026-01-30 22:43:17 -0300

    feat: adiciona mapping de endpoints, authn, authz e aplicaçao de migrations

--- KProject.Api/Program.cs
+++ KProject.Api/Program.cs
@@ -1,11 +1,36 @@
+using System.Reflection;
+using KProject.Api.Extensions;
 using KProject.Infrastructure;
+using KProject.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
 var app = builder.Build();
+
+app.MapEndpoints();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+//logs
+//exception handler
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    if ((await context.Database.GetPendingMigrationsAsync()).Any())
+    {
+        await context.Database.MigrateAsync();
+    }
+}
+
+await app.RunAsync();
