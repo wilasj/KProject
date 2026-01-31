2aa970fb5747af5320e44a46633f324d84fda957
William Alves <willoandrade29@gmail.com>
2026-01-30 22:18:12 -0300

    feat: adiciona projetos da api e de infra

---
+++ KProject.Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace KProject.Infrastructure.Database;
+
+public sealed class AppDbContext(DbContextOptions options) : IdentityUserContext<IdentityUser<int>, int>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ KProject.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,62 @@
+﻿using KProject.Infrastructure.Database;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KProject.Infrastructure;
+
+public static class DependencyInjection
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddInfrastructure(IConfiguration configuration) => services
+            .AddDatabase(configuration)
+            .AddIdentity()
+            .AddAuthentication()
+            .AddAuthorization();
+
+        private IServiceCollection AddDatabase(IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("Database");
+
+                options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+            });
+
+            return services;
+        }
+
+        private IServiceCollection AddIdentity()
+        {
+            //IdentityCore adiciona o basico que a gente precisa
+            //SignInManager porque o IdentityCore nao tem e a gente precisa pra gerar o cookie/bearer automaticamente
+            //AddEntityFrameworkStores adiciona as tabelas necessarias baseadas em quem o AppDbContext herda - nesse caso, IdentityUserContext
+        
+            services
+                .AddIdentityCore<IdentityUser<int>>()
+                .AddSignInManager<SignInManager<IdentityUser<int>>>()
+                .AddEntityFrameworkStores<AppDbContext>();
+        
+            return services;
+        }
+
+        private IServiceCollection AddAuthentication()
+        {
+            //Precisamos usar os schemes do Identity porque eh o que o SignInManager usa pra realizar os processos
+        
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme =
+                        IdentityConstants.ApplicationScheme;
+                    options.DefaultChallengeScheme =
+                        IdentityConstants.ApplicationScheme;
+                })
+                .AddCookie(IdentityConstants.ApplicationScheme, options => { options.LoginPath = "/login"; });
+
+            return services;
+        }
+    }
+}
