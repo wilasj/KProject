eda3a68608a860dd149a1076020a1a78b3eae643
William Alves <willoandrade29@gmail.com>
2026-03-04 22:27:42 -0300

    feat: adiciona logging

--- KProject.Api/Program.cs
+++ KProject.Api/Program.cs
@@ -1,10 +1,12 @@
 using System.Reflection;
+using System.Security.Claims;
 using KProject.Api.Extensions;
 using KProject.Application;
 using KProject.Infrastructure;
 using KProject.Infrastructure.Shared;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,22 +18,36 @@ builder.Services
 
 builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
 
+builder.Host.UseSerilog((context, config) =>
+{
+    config.ReadFrom.Configuration(context.Configuration);
+});
+
 var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.UseSerilogRequestLogging(opts =>
+{
+    opts.MessageTemplate = "HTTP {RequestMethod} {RequestPath} - {StatusCode} - ({Elapsed:0.000}ms)";
+    opts.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+    {
+        diagnosticContext.Set("UserId", httpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        diagnosticContext.Set("UserAgent", httpContext.Request.Headers.UserAgent.ToString());
+    };
+});
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapFallbackToFile("index.html");
 
 var api = app.MapGroup("/api");
 
 app.MapEndpoints(api);
 
-//logs
 //exception handler
-
-app.UseStaticFiles();
-
-app.MapFallbackToFile("index.html");
-
-app.UseAuthentication();
-
-app.UseAuthorization();
 
 using (var scope = app.Services.CreateScope())
 {
