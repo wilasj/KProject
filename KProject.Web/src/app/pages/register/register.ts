384f2df5d23ea1ddcecb94fbff63422046e2bba2
William Alves <willoandrade29@gmail.com>
2026-03-04 23:38:58 -0300

    feat: redesign da página de registro

--- KProject.Web/src/app/pages/register/register.ts
+++ KProject.Web/src/app/pages/register/register.ts
@@ -5,10 +5,7 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 
 @Component({
   selector: 'app-register',
-  imports: [
-    RouterLink,
-    ReactiveFormsModule
-  ],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
@@ -16,29 +13,30 @@ export class Register {
   private authService = inject(Auth);
   private router = inject(Router);
   public errors = signal<ValidationError[]>([]);
+  public loading = signal(false);
 
   registerForm = new FormGroup({
     email: new FormControl('', {nonNullable: true, validators: [Validators.required, Validators.email]}),
     password: new FormControl('', {nonNullable: true, validators: [Validators.required]}),
   });
 
-  onSubmit(){
-    if(this.registerForm.valid){
+  onSubmit() {
+    if (this.registerForm.valid) {
       const {email, password} = this.registerForm.getRawValue();
+      this.loading.set(true);
+      this.errors.set([]);
 
-      this.authService
-        .register(email, password)
-        .subscribe({
-          next: (result) => {
-            if(result.success){
-              //TODO: implementar toast de sucesso aqui
-              this.router.navigate(['/login']);
-            }
-            else {
-              this.errors.set(result.errors);
-            }
+      this.authService.register(email, password).subscribe({
+        next: (result) => {
+          if (result.success) {
+            this.router.navigate(['/login']);
+          } else {
+            this.loading.set(false);
+            this.errors.set(result.errors);
           }
-        });
+        },
+        error: () => this.loading.set(false),
+      });
     }
   }
 }
